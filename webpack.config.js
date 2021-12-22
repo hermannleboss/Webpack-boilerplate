65273ed616bd228f5d7a59f8893d6fcdb5d29700
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-22 22:22:56 +0000

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
     entry: {
         index: {
             import: './src/index.js',
-            dependOn: 'shared',
-        },
-        print: {
-            import: './src/print.js',
             dependOn: 'shared',
         },
         shared: 'lodash',
@@ -19,18 +16,27 @@ module.exports = {
         static: './dist',
     },
     plugins: [
-        new HtmlWebpackPlugin({hash: true,
+        new HtmlWebpackPlugin({
+            hash: true,
             title: 'My Awesome application',
             myPageHeader: 'Hello World',
             template: './src/index.html',
             filename: './index.html' //relative to root of the application
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css",
         }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
+        publicPath: process.env.NODE_ENV !== "production"
+            ? '/'
+            : './',
     },
     optimization: {
         runtimeChunk: 'single',
@@ -39,11 +45,20 @@ module.exports = {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== "production"
+                        ? "style-loader"
+                        : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+            }, {
+                test: /\.html$/i,
+                loader: "html-loader",
             },
         ],
     },
